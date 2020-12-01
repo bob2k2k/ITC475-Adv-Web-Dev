362426726b5a1d55dadcf37dc66bc34bdc1a4e3c
Bobby Asher <blasher1s@gmail.com>
2020-12-01 10:06:38 -0600

    MySQL Addition

--- itc475-adv-web-dev/public/admin.php
+++ itc475-adv-web-dev/public/admin.php
@@ -52,60 +52,17 @@
       </script>
     
 
+    <?php
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "formDB";
     
-    <p class="confirm">Thank you for submitting your travel agent contact request! Here is the information you submitted:</p>
-  <div id="results">
-    
-    <ol>
-        <li>First Name: <?php echo $_POST["firstName"]?></li> 
-        <br>
-        <li>Last Name: <?php echo $_POST["lastName"]?></li>
-        <br>
-        <li>Email: <?php echo $_POST["email"]?></li>
-        <br>
-        <li>Phone: <?php echo $_POST["phoneNum"]?></li>
-        <br>
-        <li>Party Size: <?php echo $_POST["partySize"]?></li>
-        <br>
-        <li>Date: <?php echo $_POST["date"]?></li>
-        <br>
-        <li>Destination: <?php echo $_POST["destination"]?></li>
-        <br>
-        <li>Activities: <?php
-          $i = 0;
-          
-          foreach($_POST["activities"] as $activity){
-            echo $activity;
-            ++$i;
-          }
-          ?></li>
-    </ol>
-  </div>
-  
+    $conn = mysqli_connect ($dbhost,$dbuser,$dbpass,$db);
 
-  <?php
-      $dbhost = "localhost";
-      $dbuser = "root";
-      $dbpass = "";
-      $db = "formDB";
-      
-      $conn = mysqli_connect ($dbhost,$dbuser,$dbpass,$db);
-      
-      if(isset($_POST['submit']))
-        if(!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['phoneNum'])
-        && !empty($_POST['date']) && !empty($_POST['partySize']) && !empty($_POST['destination'])) { 
 
-          $firstName = $_POST['firstName'];
-          $lastName = $_POST['lastName'];
-          $email = $_POST['email'];
-          $phoneNum = $_POST['phoneNum'];
-          $date = $_POST['date'];
-          $partySize = $_POST['partySize'];
-          $destination = $_POST['destination'];
-          
-          $query = "INSERT INTO form_table(firstName, lastName, emailAddress, phoneNum, date, partySize, destination)
-          VALUES ('$firstName', '$lastName', '$email', '$phoneNum','$date', '$partySize', '$destination')";
     ?>
+  
   
     <footer>
         <div class="footer-text">
